3f3524984af9291e9642e61b77b1b943a62358a1
Lei <gabriel_sl19798@hotmail.com>
2020-05-18 10:15:20 +1200

    create as many as elements you want

--- helloworld.php
+++ helloworld.php
@@ -1,3 +1,5 @@
 <?php
 echo "Hello World";
 echo "Success";
+
+//https://htcs5606s12020lei.herokuapp.com/
--- week5/class1.php
+++ week5/class1.php
@@ -7,3 +7,9 @@ echo "<h1>Hello world</h1>";
 echo "<p>Hello world</p>";
 
 echo "<p style='color: red'>Hello world</p>";
+
+$i = 0; //set counter
+while ($i < 100){ //set condition
+    echo "<p>Hello</p>"; //do something if condition is true
+    $i = $i + 1; // update counter
+}

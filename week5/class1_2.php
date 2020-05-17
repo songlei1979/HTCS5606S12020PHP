fbdb06008e5ffcb6c468c2aaf11ed70fdf62ae2c
Lei <gabriel_sl19798@hotmail.com>
2020-05-18 10:38:28 +1200

    create as many as elements you want

---
+++ week5/class1_2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Create PHP in HTML</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<?php
+$i = 1;
+while ($i<100){
+    echo "<p>class is over, come back tomorrow</p>";
+    $i = $i + 1;
+}
+?>
+</body>
+</html>

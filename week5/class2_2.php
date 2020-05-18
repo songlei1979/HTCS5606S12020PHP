31e92e577b19876ce7d7291ffc115f25b5d448f9
Lei <gabriel_sl19798@hotmail.com>
2020-05-19 09:56:19 +1200

    date function

--- week5/class2_2.php
+++ week5/class2_2.php
@@ -1,6 +1,6 @@
 <?php
 //take the time from system or online
 //data function to take the time out from system or online
-$t = data("Y-m-d"); //https://www.php.net/manual/en/function.date.php
+$t = date("Y-m-d"); //https://www.php.net/manual/en/function.date.php
 $t1 = date("H:i:s");
 echo $t." ".$t1;

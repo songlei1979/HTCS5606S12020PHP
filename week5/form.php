7db70c753a357e4290d2128e14165a349f18118e
Lei <gabriel_sl19798@hotmail.com>
2020-05-19 10:30:04 +1200

    form

---
+++ week5/form.php
@@ -0,0 +1,11 @@
+<?php
+?>
+
+<form action="result.php" method="post">
+    <p>
+        mark: <input type="number" name="mark">
+    </p>
+    <p>
+        <input type="submit" value="show">
+    </p>
+</form>

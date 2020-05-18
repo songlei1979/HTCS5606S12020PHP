bbbd6eceda95f2e9bd72d2e30fd7af7af5debcd3
Lei <gabriel_sl19798@hotmail.com>
2020-05-19 10:24:28 +1200

    use hour and minutes for alert

--- week5/class2_2.php
+++ week5/class2_2.php
@@ -11,10 +11,13 @@ $hour = intval(date("H"));
 $minutes = intval(date("i"));
 echo "<p>h: ".$hour."</p>";
 echo "<p>m: ".$minutes."</p>";
-if ($hour>=10 && $minutes < 17){ //&& means and || means or
+if ($hour>=10 && $minutes <= 30){ //&& means and || means or.
+    //work before 10:20
     echo "Have good morning";
-}elseif ($hour<20){
+}elseif ($hour<=20){
+    //work before 21:00
     echo "Have a good day";
 }else{
+    //work after 21:00 to 10:20
     echo "Have a good night";
 }

da71cb20d69ee082d584d9f7f8d58c9edce9ddfb
Lei <gabriel_sl19798@hotmail.com>
2020-05-19 09:36:14 +1200

    string methods

--- week5/class2.php
+++ week5/class2.php
@@ -13,3 +13,20 @@ $y = 5;
 echo "x = ".$x;
 echo "y = ".$y;
 echo "z = x + y = ".($x + $y);
+
+$grating = "Hello world";
+//length of string
+echo "<br>".strlen($grating)." string length<br>";
+//word count -- count number of words in that string
+echo str_word_count($grating)." string word count<br>";
+// string reverse
+echo strrev($grating)." string reverse<br>";
+// string position
+echo strpos($grating, "Hello")."word position<br>";
+//string replace
+echo str_replace($grating, "Hello", "byebye")."
+word replace<br>";
+//to uppercase
+echo strtoupper($grating)."To Upper case<br>";
+//to lower
+echo strtolower($grating)."To lower case<br>";

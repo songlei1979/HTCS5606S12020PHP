721bb13b41c782aa3998ee4cca692107eb3d53d2
Lei <gabriel_sl19798@hotmail.com>
2020-05-19 09:06:28 +1200

    number variable

--- week5/class2.php
+++ week5/class2.php
@@ -7,3 +7,9 @@ echo $grating;
 echo $who;
 
 echo "<h1>" . $grating . " " . $who . "</h1>"; //dot means merge strings in php
+
+$x = 4;
+$y = 5;
+echo "x = ".$x;
+echo "y = ".$y;
+echo "z = x + y = ".($x + $y);

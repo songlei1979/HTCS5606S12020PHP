e243260cca74e2486197bfe1d314983977ef2637
Lei <gabriel_sl19798@hotmail.com>
2020-05-19 10:07:58 +1200

    use hour and minutes for alert

--- week5/class2_2.php
+++ week5/class2_2.php
@@ -1,7 +1,19 @@
 <?php
+//set default timezone
 date_default_timezone_set('Pacific/Auckland');
 //take the time from system or online
 //data function to take the time out from system or online
 $t = date("Y-m-d"); //https://www.php.net/manual/en/function.date.php
 $t1 = date("H:i:s");
 echo $t." ".$t1;
+
+$hour = date("H");
+$minutes = date("i");
+
+if ($hour>"10" && $minutes < "10"){ //&& means and || means or
+    echo "Have good morning";
+}elseif ($hour<"20"){
+    echo "Have a good day";
+}else{
+    echo "Have a good night";
+}

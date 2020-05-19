d32517fb0dc0b9c0724f50f5be254a5ab00d3ff0
Lei <gabriel_sl19798@hotmail.com>
2020-05-19 23:27:55 +1200

    check for myles

---
+++ week5/Myles.php
@@ -0,0 +1,25 @@
+<?php
+//set default timezone
+date_default_timezone_set('Pacific/Auckland');
+//take the time from system
+//date function to tak ethe time from system or onliune
+$t = data("y-m-d"); //https://www.php.net/manual/en/function.date.php
+$t1 = date("H.i.s");
+echo $t. " ".$t1;
+
+$hour = intval(date("H"));
+$minutes = intval(date("i"));
+echo "<p>h: ".$hour."</p>";
+echo "<p>m: ".$minutes."</p>";
+
+if($hour>=12 && $minutes <=55) {  //&&means and || means or
+    //work before 1pm
+    echo "have a good morning";
+}elseif ($hour<20) {
+    //work before 2100
+    echo "have a good day";
+
+}else {
+    //work after 2100 to 1020
+    echo "have a good night";
+}

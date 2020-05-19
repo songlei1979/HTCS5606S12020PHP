dfb258aecc219fb818c60681f36c55a1de0d0536
Lei <gabriel_sl19798@hotmail.com>
2020-05-20 09:57:34 +1200

    calculate area

--- week5/calculate.php
+++ week5/calculate.php
@@ -2,8 +2,16 @@
 if (isset($_POST["width"])){ //isset checks the post data
     $w = $_POST["width"];
     $h = $_POST["height"];
-    $area = $w * $h;
-    echo "Area: ".$area;
+    $a = calculateArea($w, $h); // corresponding to my function input width and height
+    echo "Area: ".$a;
 }else{
     echo "go back to form page";
+}
+
+
+//$area = $w * $h; // formula
+//f(x,y) = x * y
+function calculateArea($width, $height){ //parameters for input
+    $area = $width * $height;
+    return $area;
 }

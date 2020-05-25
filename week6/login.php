dc1ad79fe9a10202a03e46c67c0283a1167fb26b
Lei <gabriel_sl19798@hotmail.com>
2020-05-26 09:36:47 +1200

    login.php

---
+++ week6/login.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Author: Lei Song
+ * Date: 26 May 2020
+ * Version: 1.0
+ * Purpose: for login
+ */
+
+if (isset($_POST["username"])){
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    $server = "dno6xji1n8fm828n.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    $user = "frn8ql19w9jrd0hy";
+    $pwd = "uvsw3rnopxb2uxq2";
+    $database = "eq8apmg97080j54s";
+
+    $connection = new mysqli($server, $user, $pwd, $database); //create database connection
+    if ($connection->connect_error){
+        echo $connection->connect_error;
+    }else{
+        echo "Connection Created";
+    }
+
+    // is the username in my table
+    $sql = "select * from Users where username = '$username'"; //this is our query
+    $result = $connection->query($sql); //run query on this connection through method query()
+    if ($result->num_rows == 1){ // means user exist in our database
+        while ($row = $result->fetch_assoc()){
+            if ($row["password"] == $password){ //check password
+                echo "access granted";
+            }else{
+                echo "wrong password";
+            }
+        }
+    } else{
+        echo "wrong username";
+    }
+
+
+}
+
+

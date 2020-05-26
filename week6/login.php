cfae9b1c5ebabf95582bd5fbd1fa69dd87b3ccd9
Lei <gabriel_sl19798@hotmail.com>
2020-05-27 09:50:19 +1200

    changepassword and profile

--- week6/login.php
+++ week6/login.php
@@ -32,7 +32,7 @@ if (isset($_POST["username"])){
                 // if login, we allow user to do something
                 ?>
                 <p><a href="profile.php">profile</a></p>
-                <p><a href="changepassword.php">change password</a></p>
+                <p><a href="changepasswordform.php">change password</a></p>
                 <?php
             }else{
                 echo "wrong password";

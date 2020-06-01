f3e7f8d0f83ed038ea53708184004e4e3f80035f
Lei <gabriel_sl19798@hotmail.com>
2020-06-02 09:23:38 +1200

    global variable

--- week6/dbconnection.php
+++ week6/dbconnection.php
@@ -20,7 +20,23 @@ function dbconn(){
 
 function login($username, $password){
 //    return true/false return give result back and jump out from this function. Any code after return won't be run
-    $conn = dbconn();
-//    $conn->close();
-
+    $conn = dbconn(); //create database connection from function dbconn()
+    $sql = "select * from Users where username = '$username'"; //this is our query
+    $result = $conn->query($sql); //run query on the created connection through method query()
+    if ($result->num_rows == 1){ // means user exist in our database
+        while ($row = $result->fetch_assoc()){
+            if ($row["password"] == $password){ //check password
+                $conn->close();
+                echo "db connection closed before return";
+                return true; //login
+                echo "db connection closed after return";
+            }else{ //username is correct, check password
+                $conn->close();
+                return false; // cannot login
+            }
+        }
+    } else{ //for username
+        $conn->close();
+        return false; // cannot login
+    }
 }
--- week6/login.php
+++ week6/login.php
@@ -13,39 +13,7 @@ if (isset($_POST["username"])){
     include_once "dbconnection.php";
 
     // is the username in my table
-    $sql = "select * from Users where username = '$username'"; //this is our query
-    $result = $connection->query($sql); //run query on this connection through method query()
-    if ($result->num_rows == 1){ // means user exist in our database
-        while ($row = $result->fetch_assoc()){
-            if ($row["password"] == $password){ //check password
-                echo "access granted";
-                session_start();
-                $_SESSION["username"] = $username; //set session here
-                // if login, we allow user to do something
 
-            }else{
-                echo "wrong password";
-                ?>
-                <script>
-                    setTimeout(function () {
-                        window.open("loginform.php","_self"); // go to login form
-                    }, 3000);
-
-                </script>
-                <?php
-            }
-        }
-    } else{
-        echo "wrong username";
-        ?>
-        <script>
-            setTimeout(function () {
-                window.open("loginform.php","_self"); // go to login form
-            }, 3000);
-        </script>
-        <?php
-    }
-    $connection->close(); //close my connection
 
 }else{
 

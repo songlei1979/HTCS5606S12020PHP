be5430b1c4d2bfb59401a520ea77b2953c683892
Lei <gabriel_sl19798@hotmail.com>
2020-06-03 09:36:34 +1200

    show records

--- week6/dbconnection.php
+++ week6/dbconnection.php
@@ -96,3 +96,17 @@ function changePassword($username, $oldPassword, $newPassword){
     }
 }
 
+function showRecords(){
+    $connection = dbconn();
+    $sql = "select * from Users"; // create query
+    $result = $connection->query($sql); //run the query on this connection
+
+    if ($result->num_rows > 0){ //check if there is record in the result
+        while ($row = $result->fetch_assoc()){ //show each associated row
+            echo $row['id']." ".$row['username']." ".$row['password']." ".$row['name']."<br>"; // in each row, we have columns.
+        }
+    }else{
+        echo "no result in the table";
+    }
+    $connection->close();
+}

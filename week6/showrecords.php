72320ccc7797825f926b90e988a841171bce8e43
Lei <gabriel_sl19798@hotmail.com>
2020-06-03 10:05:05 +1200

    show records

--- week6/showrecords.php
+++ week6/showrecords.php
@@ -1,5 +1,10 @@
 <?php
 include_once "dbconnection.php";
 $records = showRecords();
-print_r($records);
+//print_r($records);
+
+$i = 0;
+while ($i < sizeof($records)){
+    print_r($records[$i]);
+}
 

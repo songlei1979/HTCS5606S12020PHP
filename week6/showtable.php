50a19c81c665d2280c7d6998c6356065e75f10a5
Lei <gabriel_sl19798@hotmail.com>
2020-06-03 10:22:25 +1200

    show records

--- week6/showtable.php
+++ week6/showtable.php
@@ -13,8 +13,25 @@
         <th>Name</th>
     </tr>
     <?php
+    include_once "dbconnection.php";
+    $records = showRecords();
+    //print_r($records);
 
-    showRecords();
+    $i = 0;
+    while ($i < sizeof($records)){
+//    print_r($records[$i]); //arrayname[index] --> value
+        $record = $records[$i]; //inside each record, we have place 0 for id, place 1 for username, place 2 for password, place 3 for name
+        ?>
+        <tr>
+            <td><?php echo $record[0]; ?></td>
+            <td><?php echo $record[1]; ?></td>
+            <td><?php echo $record[2]; ?></td>
+            <td><?php echo $record[3]; ?></td>
+        </tr>
+        <?php
+        $i = $i + 1;
+    }
+
     ?>
 </table>
 </body>

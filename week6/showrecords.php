0402061419338c97e97783aa0cff63633af57589
Lei <gabriel_sl19798@hotmail.com>
2020-06-04 09:30:40 +1200

    User class

--- week6/showrecords.php
+++ week6/showrecords.php
@@ -8,7 +8,7 @@ while ($i < sizeof($records)){
 //    print_r($records[$i]); //arrayname[index] --> value
     $record = $records[$i]; //inside each record, we have place 0 for id, place 1 for username, place 2 for password, place 3 for name
     ?>
-    <p><?php $record->id ?>
+    <p><?php echo $record->id; ?>
         <?php //echo $record->username; ?>
         <?php //echo $record->password; ?>
         <?php echo $record->name; ?>

9107d0af935b813785a61ca1ebcfd5f0e9c1c5ea
Lei <gabriel_sl19798@hotmail.com>
2020-06-04 10:44:52 +1200

    User class

--- week6/createUser.php
+++ week6/createUser.php
@@ -1,5 +1,9 @@
 <?php
 include_once "class/User.php";
-
-$newUser = new User(0,'username', 'password', 'name');
+$username = $_POST['username'];
+$password = $_POST['pwd'];
+$name = $_POST['name'];
+//create an object from class User
+$newUser = new User(0,$username, $password, $name );
+//use the method save()
 $newUser->save();
---
+++ week6/creatuserform.php
@@ -0,0 +1,6 @@
+<form action="createUser.php" method="post">
+    <p>username: <input type="text" name="username"></p>
+    <p>password: <input type="password" name="pwd"></p>
+    <p>name: <input type="text" name="name"></p>
+    <button>Add</button>
+</form>

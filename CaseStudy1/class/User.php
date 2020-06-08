03f09e199d61dd6562e4776a1f10ba8b076e1c9f
Lei <gabriel_sl19798@hotmail.com>
2020-06-09 08:56:20 +1200

    Class

--- CaseStudy1/class/User.php
+++ CaseStudy1/class/User.php
@@ -1,7 +1,32 @@
 <?php
-
+/**
+ * Class User
+ * Author: Lei Song
+ * Date: 2020-06-09
+ * Version: 1
+ */
 
 class User
 {
+    public $id;
+    public $username;
+    private $password;
+    public $name;
+
+    /**
+     * User constructor.
+     * @param $id
+     * @param $username
+     * @param $password
+     * @param $name
+     */
+    public function __construct($id, $username, $password, $name)
+    {
+        $this->id = $id;
+        $this->username = $username;
+        $this->password = $password;
+        $this->name = $name;
+    }
+
 
 }

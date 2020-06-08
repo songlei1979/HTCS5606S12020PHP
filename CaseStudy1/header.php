64f93d9025f29859aca9863de018365c49eee145
Lei <gabriel_sl19798@hotmail.com>
2020-06-09 10:11:23 +1200

    use class User

--- CaseStudy1/header.php
+++ CaseStudy1/header.php
@@ -14,7 +14,7 @@
             $i = 0;
             while ($i < sizeof($categories)){
                 $category = $categories[$i];
-                echo "<li><a>".$category->name."</a></li>";
+                echo "<li><a href='products.php?categoryID=".$category->id."'>".$category->name."</a></li>";
                 $i = $i + 1;
             }
             ?>

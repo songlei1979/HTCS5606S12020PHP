c36f06a80046285dd41823a9799b5bca211cd92f
Lei <gabriel_sl19798@hotmail.com>
2020-06-09 08:49:28 +1200

    Class

--- CaseStudy1/class/Category.php
+++ CaseStudy1/class/Category.php
@@ -1,7 +1,27 @@
 <?php
-
+/**
+ * Class Category
+ * Author: Lei Song
+ * Date: 2020-06-09
+ * Version: 1
+ */
 
 class Category
 {
+    public $id;
+    public $name;
+    public $picture;
 
+    /**
+     * Category constructor.
+     * @param $id
+     * @param $name
+     * @param $picture
+     */
+    public function __construct($id, $name, $picture)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->picture = $picture;
+    }
 }
--- CaseStudy1/class/Product.php
+++ CaseStudy1/class/Product.php
@@ -1,7 +1,35 @@
 <?php
-
+/**
+ * Class Product
+ * Author: Lei Song
+ * Date: 2020-06-09
+ * Version: 1
+ */
 
 class Product
 {
+    public $id;
+    public $name;
+    public $price;
+    public $description;
+    public $categoryID;
+
+    /**
+     * Product constructor.
+     * @param $id
+     * @param $name
+     * @param $price
+     * @param $description
+     * @param $categoryID
+     */
+    public function __construct($id, $name, $price, $description, $categoryID)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->price = $price;
+        $this->description = $description;
+        $this->categoryID = $categoryID;
+    }
+
 
 }
